d1e54c057d3c30c9269c107ebb7546fc9cb39b1a
Chris Marslender <chris@chia.net>
2021-07-27 12:24:37 -0700

    Additional userify configs (#7)
    * Additiional userify configs
    
    * Fix filename
    
    * Run userify on systemd timer every minute

---
+++ userify/templates/userify_config.py
@@ -0,0 +1,19 @@
+# Userify Shim Configuration
+
+company="ChiaNetwork"
+project="Default"
+
+# This file sourced by both Python and Bash scripts, so please ensure changes
+# are loadable by each.
+
+# Enable this for additional verbosity in /var/log/userify-shim.log
+debug=0
+
+# Enable this to not actually make changes.
+# This can also be used to temporary disable the shim.
+dry_run=0
+
+# Userify Enterprise/Pro licenses
+shim_host="configure.userify.com"
+static_host="static.userify.com"
+self_signed=0
