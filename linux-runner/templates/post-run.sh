e455f62bb253a977a7685c48b2459a473ed99862
Chris Marslender <chrismarslender@gmail.com>
2023-06-27 14:05:01 -0500

    Add post run script that cleans up the home directory aside from a specific list of allowed files

---
+++ linux-runner/templates/post-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+shopt -s dotglob  # Enable matching hidden files
+
+directory="/home/{{ runner_user }}"
+exclusions=({% for item in runner_post_run_exclusions %}
+"{{ item }}"{% if not loop.last %} {% endif %}
+{% endfor %})  # Any file/directory that matches this list will not be touched
+
+for file in "$directory"/*; do
+    filename=$(basename "$file")
+    if [[ ! "${exclusions[@]}" =~ $filename ]]; then
+        echo "$filename"
+    fi
+done
