b02b70ce471f949840c517cb0159c42a6464eef7
Chris Marslender <chrismarslender@gmail.com>
2024-05-23 13:18:25 -0500

    Add post run script for mac

---
+++ mac-runner/templates/post-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+shopt -s dotglob # Enable matching hidden files
+
+directory="/home/{{ runner_user }}"
+exclusions=({% for item in runner_post_run_exclusions %}
+	"{{ item }}"{% if not loop.last %} {% endif %}
+	{% endfor %}) # Any file/directory that matches this list will not be touched
+
+for file in "$directory"/*; do
+	filename=$(basename "$file")
+	if [[ ! "${exclusions[*]}" =~ $filename ]]; then
+		sudo rm -rf "$directory/$filename"
+	fi
+done
+
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+	parent=$(dirname "$GITHUB_WORKSPACE")
+	sudo rm -rf "$parent"
+fi
+
+sudo rm -rf "/home/{{ runner_user }}/actions-runner/_work" || true
