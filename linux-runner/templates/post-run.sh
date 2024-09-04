3fdc8a45325d89092a89f33201bf31846b670ef4
Chris Marslender <chrismarslender@gmail.com>
2024-09-04 12:50:16 -0500

    Add a retry mechanism to linux cleanup

--- linux-runner/templates/post-run.sh
+++ linux-runner/templates/post-run.sh
@@ -7,16 +7,41 @@ exclusions=({% for item in runner_post_run_exclusions %}
 	"{{ item }}"{% if not loop.last %} {% endif %}
 	{% endfor %}) # Any file/directory that matches this list will not be touched
 
+# Function to delete files with a retry mechanism
+delete_with_backoff() {
+	local target="$1"
+	local retries=5
+	local delay=5
+	local attempt=1
+
+	while [[ $attempt -le $retries ]]; do
+		if sudo rm -rf "$target"; then
+			echo "Successfully deleted $target"
+			break
+		else
+			echo "Failed to delete $target, retrying in $delay seconds (Attempt $attempt/$retries)"
+			sleep $delay
+			# Exponentially increasing the delay
+			delay=$((delay * 2))
+		fi
+		attempt=$((attempt + 1))
+	done
+
+	if [[ $attempt -gt $retries ]]; then
+		echo "Failed to delete $target after $retries attempts."
+	fi
+}
+
 for file in "$directory"/*; do
 	filename=$(basename "$file")
 	if [[ ! "${exclusions[*]}" =~ $filename ]]; then
-		sudo rm -rf "$directory/$filename"
+		delete_with_backoff "$directory/$filename"
 	fi
 done
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
 	parent=$(dirname "$GITHUB_WORKSPACE")
-	sudo rm -rf "$parent"
+	delete_with_backoff "$parent"
 fi
 
-sudo rm -rf "/home/{{ runner_user }}/actions-runner/_work" || true
+delete_with_backoff "/home/{{ runner_user }}/actions-runner/_work" || true
