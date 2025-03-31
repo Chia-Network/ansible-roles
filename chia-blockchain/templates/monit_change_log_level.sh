1db1908a9dae0d902ed93661b02152a64d80bae6
Patrick Maslana <pmaslana3@gmail.com>
2025-03-31 15:54:31 -0700

    Add a way to adjust the log level before a restart occurs

---
+++ chia-blockchain/templates/monit_change_log_level.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o pipefail
+CHIA_PATH="/home/ubuntu/chia-blockchain"
+
+if [ -d "$CHIA_PATH" ]; then
+    echo "Found $CHIA_PATH — activating environment..."
+    . "$CHIA_PATH/activate"
+else
+    echo "$CHIA_PATH not found — Chia is installed with apt. Continuing...."
+fi
+
+# Set log level to DEBUG, wait, then revert to INFO
+chia configure --log-level DEBUG
+echo "Log level set to DEBUG. Restarting chia services...."
+sudo systemctl restart chia.target
+sleep 1
+chia configure --log-level INFO
+echo "Log level set to INFO. Restarting chia services...."
+sudo systemctl restart chia.target
