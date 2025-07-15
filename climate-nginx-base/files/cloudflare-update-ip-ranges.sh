750ad44c9d68374e8094b0c80a4e68150d12cbd4
Zachary Brown <zachary.g.brown@gmail.com>
2025-07-15 10:24:16 -0700

    bash formatting per linter requirements

--- climate-nginx-base/files/cloudflare-update-ip-ranges.sh
+++ climate-nginx-base/files/cloudflare-update-ip-ranges.sh
@@ -16,29 +16,28 @@ CF_URL_IP6="https://www.cloudflare.com/ips-v6"
 truncate -s 0 $CF_NGINX_TMP
 
 # Download the files.
-if [ -f /usr/bin/curl ];
-then
-	echo "# IPv4" >> $CF_NGINX_TMP
-	curl --silent $CF_URL_IP4 | sed -e 's/^.\+$/set_real_ip_from \0;/' >> $CF_NGINX_TMP
-	echo "" >> $CF_NGINX_TMP
-	echo "# IPv6" >> $CF_NGINX_TMP
-	curl --silent $CF_URL_IP6 | sed -e 's/^.\+$/set_real_ip_from \0;/' >> $CF_NGINX_TMP
+if [ -f /usr/bin/curl ]; then
+	echo "# IPv4" >>$CF_NGINX_TMP
+	curl --silent $CF_URL_IP4 | sed -e 's/^.\+$/set_real_ip_from \0;/' >>$CF_NGINX_TMP
+	echo "" >>$CF_NGINX_TMP
+	echo "# IPv6" >>$CF_NGINX_TMP
+	curl --silent $CF_URL_IP6 | sed -e 's/^.\+$/set_real_ip_from \0;/' >>$CF_NGINX_TMP
 else
 	echo "Unable to download CloudFlare files."
 	exit 1
 fi
 
-echo "" >> $CF_NGINX_TMP
-echo "real_ip_header CF-Connecting-IP;" >> $CF_NGINX_TMP
+echo "" >>$CF_NGINX_TMP
+echo "real_ip_header CF-Connecting-IP;" >>$CF_NGINX_TMP
 
-echo "" >> $CF_NGINX_TMP
-echo "# Ignore trusted IPs" >> $CF_NGINX_TMP
-echo "real_ip_recursive on;" >> $CF_NGINX_TMP
+echo "" >>$CF_NGINX_TMP
+echo "# Ignore trusted IPs" >>$CF_NGINX_TMP
+echo "real_ip_recursive on;" >>$CF_NGINX_TMP
 
 mv $CF_NGINX_TMP $CF_NGINX_CONFIG
 
 # Reload Nginx
-if sudo nginx -t &> /dev/null; then
+if sudo nginx -t &>/dev/null; then
 	echo "Nginx configuration test successful. Restarting Nginx..."
 	# Restart Nginx service
 	sudo systemctl reload nginx
