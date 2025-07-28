47c5aab3e258d38987aac1c35e0c5cf19a7c384d
Zachary Brown <zachary.g.brown@gmail.com>
2025-07-28 11:38:13 -0700

    Change target for blocklist file includes

--- climate-nginx-base/files/uptimerobot-update-ip.sh
+++ climate-nginx-base/files/uptimerobot-update-ip.sh
@@ -6,8 +6,12 @@
 # Stop if any error occurs
 set -ex
 
+BASE_DIR="/etc/nginx/blocklist-includes"
+
+mkdir -p $BASE_DIR
+
 # Location of the nginx config file that contains the UptimeRobot IP addresses.
-UPTIMEROBOT_NGINX_CONFIG="/etc/nginx/conf.d/0-uptimerobot-ips.conf"
+UPTIMEROBOT_NGINX_CONFIG="$BASE_DIR/0-uptimerobot-ips.conf"
 
 # Create a unique temporary file with proper permissions
 UPTIMEROBOT_NGINX_TMP=$(mktemp /tmp/uptimerobot_tmp.XXXXXX)
